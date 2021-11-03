1fba62a9506f6a9a9a1d6b0185edd92b38cf1b7d
Edris_Azimi <Edris.azimi.pro@gmail.com>
2021-11-03 13:04:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+// input variable created
+const input = document.getElementById("input");
+
+// Event listner wrapped in a function addingEventListener()
 function addingEventListener() {
+  input.addEventListener("click", function (event) {
+    alert("I was clicked!");
+  });
 }
+addingEventListener();
